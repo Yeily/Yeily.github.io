e82eb05aa877f8b9c0e73b521485c5262dd43173
Yeily <31491344+Yeily@users.noreply.github.com>
2021-05-05 17:51:41 -0600

    Create global.js

---
+++ js/global.js
@@ -0,0 +1,14 @@
+/*
+Autor: Yeily Calderón M.
+Mail: yeilycalderon@yahoo.es
+*/
+
+var g_dirRelativa = window.URL_RELATIVA;
+
+document.oncontextmenu = function() {
+  return false;
+};
+
+document.onmousedown = function(ctl) {
+  
+}
